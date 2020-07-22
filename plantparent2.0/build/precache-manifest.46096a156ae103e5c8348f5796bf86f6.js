8730b2954259d61248946f87d043a9f52ba3f617
MeerKatnip <amy.roy@icloud.com>
2020-07-22 08:42:20 -0500

    created build copy and modified build script in package.json

---
+++ plantparent2.0/build/precache-manifest.46096a156ae103e5c8348f5796bf86f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617f93fc8f80010506e2dd15a4b2a1a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "700bac52636e69cadc71",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f634b4b0e960b85094d4",
+    "url": "/static/css/main.759b3f44.chunk.css"
+  },
+  {
+    "revision": "700bac52636e69cadc71",
+    "url": "/static/js/2.dc677db6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dc677db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f634b4b0e960b85094d4",
+    "url": "/static/js/main.1d78c5b5.chunk.js"
+  },
+  {
+    "revision": "fcadc11334918ba4bbdd",
+    "url": "/static/js/runtime-main.a7d448df.js"
+  },
+  {
+    "revision": "e5da9cfa899cc9c15703ce0ecfdc88b2",
+    "url": "/static/media/greenbg.e5da9cfa.jpg"
+  }
+]);
